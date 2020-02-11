7cc3f279f3af92a99fbb3a4d8f2e4c0ab45e24bb
leonmezu1 <leo.mez@outlook.com>
2020-02-11 17:32:01 -0500

    Add style to show template

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,7 +9,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = 'Article was sucesfully created'
+      flash[:success] = 'Article was sucesfully created'
       redirect_to articles_path(@article)
     else
       render 'new'
@@ -20,7 +20,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
-    flash[:notice] = 'Article was succesfully deleted'
+    flash[:danger] = 'Article was succesfully deleted'
     redirect_to articles_path
   end
 
@@ -32,7 +32,7 @@ class ArticlesController < ApplicationController
 
   def update
     if @article.update(article_params)
-      flash[:notice] = 'Article was succesfully updated'
+      flash[:success] = 'Article was succesfully updated'
       redirect_to article_path(@article)
     else
       render 'edit'
