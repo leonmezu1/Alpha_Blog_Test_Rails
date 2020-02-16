4329b0a8715f7c30ad7d6d8180b464211241da62
leonmezu1 <leo.mez@outlook.com>
2020-02-16 11:33:57 -0500

    Added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  validates :username, presence: true, length: { minimum: 2, maximum: 20 },
+                       uniqueness: { case_sensitive: false }
+  validates :email, presence: true, length: { minimum: 2, maximum: 20 },
+                    uniqueness: { case_sensitive: false },
+                    format: { with: VALID_EMAIL_REGEX }
 end
